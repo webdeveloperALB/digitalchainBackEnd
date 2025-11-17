f8dc7b739f35be16d4ed2ad905270e9a7b700422
Kevin Koltraka <kevikoltraka157@gmail.com>
2025-11-17 18:50:04 +0100

    changes

--- components/dashboard/transaction-history.tsx
+++ components/dashboard/transaction-history.tsx
@@ -249,8 +249,6 @@ export default function ClientDepositsView() {
                             day: "2-digit",
                             month: "2-digit",
                             year: "numeric",
-                            hour: "2-digit",
-                            minute: "2-digit",
                           })}
                         </p>
                       </div>
